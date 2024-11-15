90a12d6c366ff2561c8cada19f13f66814996f78
adrianrv007 <adrianrovil007@gmail.com>
2024-11-15 13:32:10 +0100

    Actualizado

---
+++ Coordenadas.java
@@ -0,0 +1,33 @@
+package practica2;
+
+/**
+ *
+ * @author adrianrv007
+ */
+public class Coordenadas {
+	private int x, y;
+
+	public Coordenadas(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX()
+	{
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	
+}
